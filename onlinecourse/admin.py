3b6133971c68fa49d82130675a2eb1bdaf9c8602
SophMarch <soph.march@gmail.com>
2021-08-18 09:35:04 +0000

    update model and templates app

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
+from .models import Question, Choice
 from .models import Course, Lesson, Instructor, Learner
-
-# <HINT> Register QuestionInline and ChoiceInline classes here
 
 
 class LessonInline(admin.StackedInline):
@@ -10,7 +8,16 @@ class LessonInline(admin.StackedInline):
     extra = 5
 
 
-# Register your models here.
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 4
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4 
+
+
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
@@ -22,9 +29,13 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
